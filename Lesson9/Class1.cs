d838b2a4c2f9479145b2545a1dbd0616dc27e957
Akiva Cohen <akiva.coen@gmail.com>
2024-09-15 19:55:27 +0300

    save

---
+++ Lesson9/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9
+{
+    public class Class1
+    {
+        public static int CountMember(Queue<int> queue)
+        {
+            Queue<int> temp = new Queue<int>();
+            int count = 0;
+
+            while (!queue.IsEmpty())
+            {
+                count++;
+                temp.Insert(queue.Remove());
+            }
+
+            while (!temp.IsEmpty())
+                queue.Insert(temp.Remove());
+
+
+            return count;
+        }
+
+        public static void Run()
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Insert(1);
+            queue.Insert(2);
+            queue.Insert(3);
+            queue.Insert(4);
+            queue.Insert(5);
+
+            Console.WriteLine("queue:");
+            Console.WriteLine(queue);
+
+            Console.WriteLine(CountMember(queue));
+
+        }
+    }
+}
---
+++ Lesson9/Program.cs
@@ -0,0 +1,10 @@
+﻿using Lesson9;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Class1.Run();
+    }
+}
